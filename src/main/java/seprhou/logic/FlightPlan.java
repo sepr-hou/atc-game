07aaa5428d173fd9f66c3570ebe417c0285f9e9b
Rob Kerry <rck504@york.ac.uk>
2013-11-19 12:27:17 +0000

    Add FlightPlan class
    Added the class "FlightPlan"

---
+++ src/main/java/seprhou/logic/FlightPlan.java
@@ -0,0 +1,23 @@
+package seprhou.logic;
+import java.util.List;
+public class FlightPlan implements Iterable<Waypoint> {
+	
+	private Waypoint[] waypoints;
+	public FlightPlan(Waypoint[] waypoints)
+	{
+		
+	}
+	public int size()
+	{
+		
+	}
+	public List<Waypoint> toList()
+	{
+		
+	}
+	public Waypoint[] toArray()
+	{
+		
+	}
+
+}
