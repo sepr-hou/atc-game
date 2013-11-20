a4f13b97a6c9558723b7d45fdda3560160da45c8
Rob Kerry <rck504@york.ac.uk>
2013-11-20 13:45:54 +0000

    Waypoint updated
    Adds documentation for waypoint class

--- src/main/java/seprhou/logic/Waypoint.java
+++ src/main/java/seprhou/logic/Waypoint.java
@@ -1,36 +1,47 @@
 package seprhou.logic;
 
+/**
+ * An immutable subclass of FlightPlan
+ *
+ */
+
 public class Waypoint {
 	private Vector2D position;
 	private float speed, altitude;
 	
+	/**
+	 * Constructs a new waypoint with the given position, speed and altitude.
+	 * @param position the location of the waypoint in the airspace
+	 * @param speed the speed of the aircraft at this waypoint
+	 * @param altitude the altitude of the aircraft at this waypoint
+	 */
 	public Waypoint(Vector2D position, float speed, float altitude)
 	{
 	this.position = position;
 	this.speed = speed;
 	this.altitude = altitude;
 	}
-
+	/** returns the position of the waypoint*/
 	public Vector2D getPosition() {
 		return position;
 	}
-
+	/** changes the position of the waypoint*/
 	public void setPosition(Vector2D position) {
 		this.position = position;
 	}
-
+	/** returns the speed of the aircraft at the waypoint*/
 	public float getSpeed() {
 		return speed;
 	}
-
+	/** changes the speed of the aircraft at the waypoint*/
 	public void setSpeed(float speed) {
 		this.speed = speed;
 	}
-
+	/** returns the altitude of the aircraft at the waypoint*/
 	public float getAltitude() {
 		return altitude;
 	}
-
+	/** changes the altitude of the aircraft at the waypoint*/
 	public void setAltitude(float altitude) {
 		this.altitude = altitude;
 	}
