378fc9ec44ce10ce25de06c3b7cc65a542e5b036
Jack Knott <jack.knott@blueyonder.co.uk>
2013-11-20 13:41:30 +0000

    Rectangle.java created

---
+++ src/main/java/seprhou/logic/Rectangle.java
@@ -0,0 +1,35 @@
+package seprhou.logic;
+
+public class Rectangle
+{
+	private final Vector2D p1, p2;
+	
+	public Rectangle(Vector2D point1, Vector2D point2)
+	{
+		this.p1 = point1;
+		this.p2 = point2;
+	}
+	
+	public float getHeight()
+	{
+		
+	}
+	
+	public float getWidth()
+	{
+		
+	}
+	
+	public boolean contains(Vector2D point)
+	{
+		
+	}
+	
+	public boolean intersects(Rectangle other)
+	{
+	}
+	
+	public boolean intersects(Vector2D centre, float radius)
+	{
+	}
+}
