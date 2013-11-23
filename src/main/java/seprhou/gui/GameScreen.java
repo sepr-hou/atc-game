38db4a907dfa7bc85cee1aee219e9e203320d843
jk1993 <jack.knott@blueyonder.co.uk>
2013-11-23 18:43:26 +0000

    Added GameScreen

---
+++ src/main/java/seprhou/gui/GameScreen.java
@@ -0,0 +1,10 @@
+package seprhou.gui;
+
+public class GameScreen extends AbstractScreen{
+
+	public GameScreen(MainWindow mainWindow) {
+		super(mainWindow);
+		// TODO Auto-generated constructor stub
+	}
+
+}
