f99145ef1b3729554b616a8cfab4742560434f30
James Cowgill <james410@cowgill.org.uk>
2014-01-13 23:42:56 +0000

    Added a minimum of 1 second between each aircraft

--- src/main/java/seprhou/logic/DefaultObjectCreationRate.java
+++ src/main/java/seprhou/logic/DefaultObjectCreationRate.java
@@ -8,8 +8,10 @@ import java.util.Random;
 public class DefaultObjectCreationRate
 {
 	private static final float RATE_PER_SEC = 0.2f;
+	private static final float MIN_TIME = 1f;
 
 	private final Random random = new Random();
+	private float timeSinceLastAircraft;
 
 	/**
 	 * Returns true if a new object should be created this refresh
@@ -19,7 +21,19 @@ public class DefaultObjectCreationRate
 	 */
 	public boolean nextBoolean(Airspace airspace, float delta)
 	{
-		// TODO This might be "too simple" and need changing later
-		return random.nextFloat() < delta * RATE_PER_SEC;
+		timeSinceLastAircraft += delta;
+
+		// Check Minimum time between aircraft
+		if (timeSinceLastAircraft < MIN_TIME)
+			return false;
+
+		// Add some randomness
+		if (random.nextFloat() < delta * RATE_PER_SEC)
+		{
+			timeSinceLastAircraft = 0;
+			return true;
+		}
+
+		return false;
 	}
 }
