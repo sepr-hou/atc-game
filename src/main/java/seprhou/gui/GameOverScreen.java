de8d6c1d664d1d7bf667dc3deee3465a75a46c5d
Jack Knott <jack.knott@blueyonder.co.uk>
2014-01-16 13:29:10 +0000

    Created Game Over Screen

---
+++ src/main/java/seprhou/gui/GameOverScreen.java
@@ -0,0 +1,11 @@
+package seprhou.gui;
+
+public class GameOverScreen extends AbstractScreen
+{
+
+	public GameOverScreen(AtcGame game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
