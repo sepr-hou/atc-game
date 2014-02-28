cc90f3450bec99835beb3cef33947d1f1107d19c
James Cowgill <james410@cowgill.org.uk>
2014-02-28 19:51:37 +0000

    Refactored ConcreteAircraft to simplify aircraft drawing
    - Extracted the drawing method out to reduce code duplication
    - Simplified SHADOW_DIRECTION construction using Vector2D.fromPolar

--- src/main/java/seprhou/gui/ConcreteAircraft.java
+++ src/main/java/seprhou/gui/ConcreteAircraft.java
@@ -20,9 +20,7 @@ public class ConcreteAircraft extends Aircraft
 	private static final float AIRCRAFT_MAX_ALTITUDE = Collections.max(Constants.INITIAL_ALTITUDES);
 	private static final float SHADOW_HEIGHT_MULTIPLIER = 0.003f;
 	private static final float SHADOW_ANGLE = 0.4f;
-	private static final Vector2D SHADOW_DIRECTION = new Vector2D(
-			(float) Math.sin(ConcreteAircraft.SHADOW_ANGLE),
-			(float) Math.cos(ConcreteAircraft.SHADOW_ANGLE));
+	private static final Vector2D SHADOW_DIRECTION = Vector2D.fromPolar(SHADOW_HEIGHT_MULTIPLIER, SHADOW_ANGLE);
 
 	public ConcreteAircraft(String name, float weight, int crew, FlightPlan flightPlan, Airspace airspace)
 	{
@@ -34,8 +32,6 @@ public class ConcreteAircraft extends Aircraft
 	{
 		GameArea gameArea = (GameArea) state;
 		SpriteBatch batch = gameArea.getBatch();
-
-		float angleDegrees = getVelocity().getAngle() * (float) (180.0 / Math.PI);
 
 		// Add parent X and Y since SpriteBatch does not adjust coordinates for the Actor
 		float xPos = gameArea.getX() + getPosition().getX() - getSize();
@@ -63,34 +59,28 @@ public class ConcreteAircraft extends Aircraft
 			aircraftTexture = Assets.AIRCRAFT_TEXTURE;
 		}
 
-		batch.draw(Assets.AIRCRAFT_SHADOW, // Aircraft shadow texture
-				xPos + SHADOW_DIRECTION.getX()
-						* (SHADOW_HEIGHT_MULTIPLIER * altitude), // X
-																	// position
-																	// (bottom
-																	// left)
-				yPos + SHADOW_DIRECTION.getY()
-						* (SHADOW_HEIGHT_MULTIPLIER * altitude), // Y
-																	// position
-																	// (bottom
-																	// right)
-				getSize(), // X rotation origin
-				getSize(), // Y rotation origin
-				getSize() * 2, // Width
-				getSize() * 2, // Height
-				1.0f, // X scaling
-				1.0f, // Y scaling
-				angleDegrees, // Rotation
-				0, // X position in texture
-				0, // Y position in texture
-				Assets.AIRCRAFT_TEXTURE.getWidth(), // Width of source texture
-				Assets.AIRCRAFT_TEXTURE.getHeight(),// Height of source texture
-				false, // Flip in X axis
-				false // Flip in Y axis
-		);
+		// Draw aircraft + its shadow
+		drawAircraft(batch, Assets.AIRCRAFT_SHADOW,
+				xPos + SHADOW_DIRECTION.getX() * altitude,
+				yPos + SHADOW_DIRECTION.getY() * altitude);
+
+		drawAircraft(batch, aircraftTexture, xPos, yPos);
+
+		// Draw altitude in top right
+		String str = (int) this.getAltitude() + "ft";
+		Assets.FONT.draw(batch, str, xPos + this.getSize() * 2,
+				yPos + this.getSize() * 2);
+
+		String str2 = Math.round(this.getVelocity().getLength() * 10) + "mph";
+		Assets.FONT.draw(batch, str2, xPos + this.getSize() * 2, yPos + this.getSize() * 2 - 16);
+	}
+
+	private void drawAircraft(SpriteBatch batch, Texture texture, float xPos, float yPos)
+	{
+		float angleDegrees = getVelocity().getAngle() * (float) (180.0 / Math.PI);
 
 		batch.draw(
-				aircraftTexture,                    // Aircraft texture
+				texture,                            // Aircraft texture
 				xPos,                               // X position (bottom left)
 				yPos,                               // Y position (bottom right)
 				getSize(),                          // X rotation origin
@@ -107,14 +97,6 @@ public class ConcreteAircraft extends Aircraft
 				false,                              // Flip in X axis
 				false                               // Flip in Y axis
 		);
-
-		// Draw altitude in top right
-		String str = (int) this.getAltitude() + "ft";
-		Assets.FONT.draw(batch, str, xPos + this.getSize() * 2,
-				yPos + this.getSize() * 2);
-
-		String str2 = Math.round(this.getVelocity().getLength() * 10) + "mph";
-		Assets.FONT.draw(batch, str2, xPos + this.getSize() * 2, yPos + this.getSize() * 2 - 16);
 	}
 
 	@Override public float getSize()             { return Constants.AIRCRAFT_SIZE; }
