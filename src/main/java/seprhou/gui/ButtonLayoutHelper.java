e8a2d4fe529c13f2034e4cc122e0ca49316e9617
James Cowgill <james410@cowgill.org.uk>
2014-03-01 00:24:04 +0000

    Refactored menu + options screen init code
    - Moved all the common button layout code into a new ButtonLayoutHelper class

---
+++ src/main/java/seprhou/gui/ButtonLayoutHelper.java
@@ -0,0 +1,56 @@
+package seprhou.gui;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+/**
+ * Class used by the main and options menu to help the layout of buttons
+ */
+public class ButtonLayoutHelper
+{
+	private static final float BUTTON_WIDTH = 300f;
+	private static final float BUTTON_HEIGHT = 60f;
+
+	// Put buttons in the middle of the screen in the x-axis
+	private static final float BUTTON_XPOS = (AbstractScreen.SCREEN_WIDTH - BUTTON_WIDTH) / 2;
+
+	// Start placing the label and buttons 100px above the centre of the screen in the y-axis
+	private static final float BUTTON_INITIAL_YPOS = (AbstractScreen.SCREEN_HEIGHT - BUTTON_HEIGHT) / 2;
+
+	private final Stage stage;
+	private final float buttonSpacing;
+
+	private float nextYPos = BUTTON_INITIAL_YPOS;
+
+	/**
+	 * Initializes a new ButtonLayoutHelper
+	 * @param stage stage to add buttons to
+	 */
+	public ButtonLayoutHelper(Stage stage, float spacing)
+	{
+		this.stage = stage;
+		this.buttonSpacing = spacing;
+	}
+
+	/**
+	 * Creates a new button on the menu
+	 *
+	 * @param name name of the button (text as it will appear)
+	 * @param listener listener to handle click events
+	 * @return the new button
+	 */
+	public TextButton createButton(String name, ClickListener listener)
+	{
+		TextButton button = new TextButton(name, Assets.SKIN);
+
+		button.setBounds(BUTTON_XPOS, nextYPos, BUTTON_WIDTH, BUTTON_HEIGHT);
+		nextYPos -= BUTTON_HEIGHT + buttonSpacing;
+
+		if (listener != null)
+			button.addListener(listener);
+
+		stage.addActor(button);
+		return button;
+	}
+}
--- src/main/java/seprhou/gui/MenuScreen.java
+++ src/main/java/seprhou/gui/MenuScreen.java
@@ -2,20 +2,16 @@ package seprhou.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+import java.awt.*;
 
 /**
  * The screen displaying the main menu
  */
 public class MenuScreen extends AbstractScreen
 {
-	private static final float BUTTON_WIDTH = 300f;
-	private static final float BUTTON_HEIGHT = 60f;
-	private static final float BUTTON_SPACING = 10f;
-
 	/**
 	 * Initializes the menu screen
 	 *
@@ -24,79 +20,43 @@ public class MenuScreen extends AbstractScreen
 	public MenuScreen(AtcGame game)
 	{
 		super(game, false);
-		Stage stage = getStage();
-
-		// Put buttons in the middle of the screen in the x-axis
-		final float buttonX = (SCREEN_WIDTH - BUTTON_WIDTH) / 2;
-
-		// Start placing the label and buttons 100px above the centre of the screen in the y-axis
-		float currentY = ((SCREEN_HEIGHT - BUTTON_HEIGHT)/2);
-
-		// Clear the stage
-		stage.clear();
 
 		// Set background image
-		Image backgroundImage = new Image(Assets.MENU_BACKGROUND_TEXTURE);
-		stage.addActor(backgroundImage);
+		getStage().addActor(new Image(Assets.MENU_BACKGROUND_TEXTURE));
 
-		// button "Start Game"setScreen(menuScreen);
-		TextButton startGameButton = new TextButton("Start game", Assets.SKIN);
-		startGameButton.setX(buttonX);
-		startGameButton.setY(currentY);
-		startGameButton.setWidth(BUTTON_WIDTH);
-		startGameButton.setHeight(BUTTON_HEIGHT);
-		startGameButton.addListener(new ClickListener() {
+		// Add buttons
+		ButtonLayoutHelper layout = new ButtonLayoutHelper(getStage(), 10);
+
+		layout.createButton("Start game", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				getGame().showGame();
 			}
 		});
-		stage.addActor(startGameButton);
 
-		// button "Options"
-		TextButton optionsButton = new TextButton("Options", Assets.SKIN);
-		optionsButton.setX(buttonX);
-		optionsButton.setY((currentY -= BUTTON_HEIGHT + BUTTON_SPACING));
-		optionsButton.setWidth(BUTTON_WIDTH);
-		optionsButton.setHeight(BUTTON_HEIGHT);
-		optionsButton.addListener(new ClickListener() {
+		layout.createButton("Options", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				getGame().showOptions();
 			}
 		});
-		stage.addActor(optionsButton);
 
-		// button "High Scores"
-		TextButton highScoresButton = new TextButton("High Scores", Assets.SKIN);
-		highScoresButton.setX(buttonX);
-		highScoresButton.setY((currentY -= BUTTON_HEIGHT + BUTTON_SPACING));
-		highScoresButton.setWidth(BUTTON_WIDTH);
-		highScoresButton.setHeight(BUTTON_HEIGHT);
-		highScoresButton.addListener(new ClickListener() {
+		layout.createButton("High Scores", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				getGame().showHighScores();
 			}
 		});
-		stage.addActor(highScoresButton);
 
-		// button "Exit"
-		TextButton exitButton = new TextButton("Exit", Assets.SKIN);
-		exitButton.setX(buttonX);
-		exitButton.setY((currentY -= BUTTON_HEIGHT + BUTTON_SPACING));
-		exitButton.setWidth(BUTTON_WIDTH);
-		exitButton.setHeight(BUTTON_HEIGHT);
-		exitButton.addListener(new ClickListener() {
+		layout.createButton("Exit", new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y)
 			{
 				Gdx.app.exit();
 			}
 		});
-		stage.addActor(exitButton);
 	}
 }
