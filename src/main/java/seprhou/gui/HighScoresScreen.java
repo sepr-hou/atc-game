068ac72e1327db3ecce9bf832a1e96572b90e737
James Cowgill <james410@cowgill.org.uk>
2014-03-13 13:04:37 +0000

    Use libGDX tables on the highscores screen

--- src/main/java/seprhou/gui/HighScoresScreen.java
+++ src/main/java/seprhou/gui/HighScoresScreen.java
@@ -1,10 +1,13 @@
 package seprhou.gui;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
+import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 
 import seprhou.highscores.HighscoreEntry;
@@ -16,7 +19,12 @@ public class HighScoresScreen extends AbstractScreen {
 
 	private final static float SCORE_SCALE = 1.3f;
 
-	private final com.badlogic.gdx.scenes.scene2d.ui.List onScreenList;
+	private final static float TABLE_WIDTH = 600;
+	private final static float TABLE_PADDING = 10;
+
+	private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	private final Table scoresTable;
 
 	public HighScoresScreen(AtcGame game)
 	{
@@ -27,30 +35,36 @@ public class HighScoresScreen extends AbstractScreen {
 		// Set background image
 		stage.addActor(new Image(Assets.MENU_BACKGROUND_TEXTURE));
 
-		// Create on screen list
-		onScreenList = new com.badlogic.gdx.scenes.scene2d.ui.List(new Object[0], Assets.SKIN);
-		stage.addActor(onScreenList);
+		// Create table to store labels into
+		scoresTable = new Table(Assets.SKIN);
+		scoresTable.setBounds((SCREEN_WIDTH - TABLE_WIDTH) / 2, 400, TABLE_WIDTH, 500);
+		stage.addActor(scoresTable);
 	}
 
 	@Override
 	public void show()
 	{
 		super.show();
+		scoresTable.clear();
 
-		// Generate labels for all the scores
-		List<HighscoreEntry> scores = getGame().getGlobalScores().getScores();
-		Label[] labels = new Label[scores.size()];
+		// Add headings
+		scoresTable.add("Score");
+		scoresTable.add("Date");
+		scoresTable.row();
 
-		for (int i = 0; i < labels.length; i++)
+		// Add scores to the table
+		for (HighscoreEntry entry : getGame().getGlobalScores().getScores())
 		{
-			HighscoreEntry entry = scores.get(i);
-
-			labels[i] = new Label(entry.getScore() + ", " + entry.getDate(), Assets.SKIN);
-			labels[i].setFontScale(SCORE_SCALE);
-			labels[i].setAlignment(Align.center);
+			addTableLabel(entry.getScore() + "");
+			addTableLabel(DATE_FORMAT.format(entry.getDate()));
+			scoresTable.row();
 		}
+	}
 
-		// Write list to stage
-		onScreenList.setItems(labels);
+	private void addTableLabel(String str)
+	{
+		Label label = new Label(str, Assets.SKIN);
+		label.setFontScale(SCORE_SCALE);
+		scoresTable.add(label).pad(TABLE_PADDING);
 	}
 }
