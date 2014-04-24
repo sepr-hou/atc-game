9af62f50a0dfa9d55b380107c9fca4a584c6aea4
Jack Knott <jack.knott@blueyonder.co.uk>
2014-04-24 14:16:44 +0100

    Made network config screen prettier and added some buttons

--- src/main/java/seprhou/gui/NetworkConfigScreen.java
+++ src/main/java/seprhou/gui/NetworkConfigScreen.java
@@ -1,10 +1,35 @@
 package seprhou.gui;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 public class NetworkConfigScreen extends AbstractScreen{
 
 	public NetworkConfigScreen(AtcGame game) {
 		super(game);
-		// TODO Auto-generated constructor stub
+				
+		// Set background image
+		getStage().addActor(new Image(Assets.MENU_BACKGROUND_TEXTURE));
+		
+		ButtonLayoutHelper layout = new ButtonLayoutHelper(getStage(), 10);
+		
+		layout.createButton("Connect", new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y)
+			{
+				// TODO
+			}
+		});
+		
+		layout.createButton("Host", new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y)
+			{
+				// TODO
+			}
+		});
 	}
-
+	
+	
 }
