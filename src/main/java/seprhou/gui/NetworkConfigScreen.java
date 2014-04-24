5c13c0061b05cef72f8522830ca86ab41f2fb6e0
Jack Knott <jack.knott@blueyonder.co.uk>
2014-04-24 13:55:28 +0100

    Added screen for multiplayer network config

---
+++ src/main/java/seprhou/gui/NetworkConfigScreen.java
@@ -0,0 +1,10 @@
+package seprhou.gui;
+
+public class NetworkConfigScreen extends AbstractScreen{
+
+	public NetworkConfigScreen(AtcGame game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
